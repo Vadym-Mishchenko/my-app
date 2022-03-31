92efb3cbb545062592c647caacc6683616edf4e8
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-03-31 21:41:30 +0300

    Messege was created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Messege } from './Components/Messege/Messege';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Messege />
     </div>
   );
 }
---
+++ src/Components/Messege/Messege.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './Messege.css';
+
+export const Messege = () => {
+  return (
+    <div className="messege">
+      Русский военный корбль иди нах*й!
+    </div>
+  )
+};
