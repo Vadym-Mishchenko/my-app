79b792d93183cb298ce3dd618891978ddb286a90
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-04-01 12:50:35 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Form } from './Components/Messege/Form/Form';
 import { Messege } from './Components/Messege/Messege';
 
 function App() {
   return (
     <div className="App">
       <Messege />
+      <Form />
     </div>
   );
 }
