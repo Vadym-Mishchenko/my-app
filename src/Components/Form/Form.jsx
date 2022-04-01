8eaec8a7311df13243d3e70c091cbcf24e6a8124
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-04-01 16:14:17 +0300

    classname imported, Input was styled

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import cn from 'classnames';
 
 import './Form.scss';
+import classNames from "classnames";
 
 export const Form = () => {
+  const [buttonPhysicalPerson, setButtonPhysicalPerson] = useState(true);
+  const [buttonEntityPerson, setButtonEntityPerson] = useState(false);
+
+  const onClickButtonPhysicalPerson = () => {
+    setButtonEntityPerson(false);
+    setButtonPhysicalPerson(!buttonPhysicalPerson)
+  };
+
+  const onClickButtonEntityPerson = () => {
+    setButtonPhysicalPerson(false);
+    setButtonEntityPerson(!buttonEntityPerson);
+  };
+
   return (
     <div className="form">
       <h1 className="form__title">Заповнiть форму</h1>
-      <div className="form__buttons">
-        <button type="button" className="buttons__button-individual">Фiз. особа</button>
-        <button type="button" className="buttons-button-entity">Юр. особа</button>
+      <div className="form__buttons buttons">
+        <button
+          type="button"
+          className={cn(
+            "buttons__button",
+            "buttons__button-r",
+            {'buttons__button--active': buttonPhysicalPerson}
+          )}
+          onClick={onClickButtonPhysicalPerson}
+        >
+          Фiз. особа
+        </button>
+  
+        <button
+          type="button"
+          className={classNames(
+            "buttons__button",
+            "buttons__button-l",
+            {'buttons__button--active': buttonEntityPerson}
+          )}
+          onClick={onClickButtonEntityPerson}
+
+        >
+          Юр. особа
+        </button>
       </div>
 
       <div className="form__input input">
