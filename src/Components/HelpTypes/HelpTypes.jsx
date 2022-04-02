264b9946a0d6ebb707229e70411c6bc7e7f6f9fe
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-04-02 19:57:59 +0300

    card was fixed

--- src/Components/HelpTypes/HelpTypes.jsx
+++ src/Components/HelpTypes/HelpTypes.jsx
@@ -28,7 +28,50 @@ export const HelpTypes = () => {
           <p className="help__type-name">Волонтерство</p>
         </div>
       </div>
-      <div className="help__input"></div>
+
+      <div className="help__payment-choice payment-choice">
+        <div className="payment-choice__left">
+          <p className="payment-choice__left-text">Споciб оплати</p>
+          <div className="payment-choice__left-card">
+            <p className="payment-choice__left-title">VISA</p>
+          </div>
+          <div className="payment-choice__left-card payment-choice__left-card--active">
+            <p className="payment-choice__left-title">Приват24</p>
+          </div>
+          <div className="payment-choice__left-card">
+            <p className="payment-choice__left-title">Термiнали</p>
+          </div>
+          <div className="payment-choice__left-card">
+            <p className="payment-choice__left-title">WebMoney</p>
+          </div>
+          <div className="payment-choice__left-card">
+            <p className="payment-choice__left-title">PayPal</p>
+          </div>
+        </div>
+        <div className="payment-choice__right">
+          <p className="payment-choice__left-text">Введiть наступнi даннi</p>
+          <div className="payment-choice__right-card card-data">
+            <div className="card-data__number">
+              <p className="card-data__number-p">Номер карти</p>
+              <input type="text" className="card-data__number-input"/>
+              <input type="text" className="card-data__number-input"/>
+              <input type="text" className="card-data__number-input"/>
+              <input type="text" className="card-data__number-input"/>
+            </div>
+            <div className="card-data__container">
+              <div className="card-data__term">
+                <p className="card-data__term-p">Термін дії</p>
+                <input type="text" className="card-data__term-input"/>
+              </div>
+              <div className="card-data__cvv">
+                <p className="card-data__cvv-p">CVC/CVV</p>
+                <input type="text" className="card-data__cvv-input"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <button className="help__button">Допомогти</button>
     </div>
   )
